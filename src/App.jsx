75b96c1c15f8ecffdd81e0d981aecfb4ce92ec1f
Carlos Moreno <carlos.moreno.4268@gmail.com>
2025-11-17 12:55:06 -0800

    04-hooks Add Order component that uses useState hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
 import { createRoot } from "react-dom/client";
-import Pizza from "./Pizza";
+import Order from "./Order";
 
 function App() {
   return (
     <div>
       <h1>Padre Gino's - Order Now</h1>
-      <Pizza
-        name="The Pepperoni Pizza"
-        description="Basic pepperoni pizza"
-        image="/public/pizzas/pepperoni.webp"
-      />
-      <Pizza
-        name="Americano Pizza"
-        description="French fries and hot dogs"
-        image="/public/pizzas/big_meat.webp"
-      />
-      <Pizza
-        name="The Hawaiian"
-        description="Ham and pineapple"
-        image="/public/pizzas/hawaiian.webp"
-      />
-      <Pizza
-        name="Chicken Pizza"
-        description="Chicken nuggets on your pizza, wow"
-        image="/public/pizzas/ckn_alfredo.webp"
-      />
-      <Pizza
-        name="Baked Potato Pizza"
-        description="Unholy potato mash"
-        image="/public/pizzas/veggie_veg.webp"
-      />
+      <Order />
     </div>
   );
 }
