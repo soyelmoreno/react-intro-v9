7608a4325e9b20e93739a8c5b50cbe14cc4fb322
Carlos Moreno <carlos.moreno.4268@gmail.com>
2025-11-17 22:03:29 -0800

    08-context Add Header component with Cart icon showing number in cart using Context

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import PizzaOfTheDay from "./PizzaOfTheDay";
 import Order from "./Order";
+import Header from "./Header";
+import { CartContext } from "./contexts";
 
 function App() {
+  const cartHook = useState([]);
   return (
     <StrictMode>
-      <div>
-        <h1 className="logo">Padre Gino's - Order Now</h1>
-        <Order />
-        <PizzaOfTheDay />
-      </div>
+      <CartContext.Provider value={cartHook}>
+        <div>
+          <Header />
+          <Order />
+          <PizzaOfTheDay />
+        </div>
+      </CartContext.Provider>
     </StrictMode>
   );
 }
