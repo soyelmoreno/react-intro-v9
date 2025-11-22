773baf14a6d09768665947e15f60cd9d424921f5
Carlos Moreno <carlos.moreno.4268@gmail.com>
2025-11-22 00:12:55 -0800

    14-testing Add a test for the cart icon number in the Header

--- src/Header.jsx
+++ src/Header.jsx
@@ -11,7 +11,10 @@ export default function Header() {
         <h1 className="logo">Padre Gino's Pizza</h1>
       </Link>
       <div className="nav-cart">
-        🛒<span className="nav-cart-number">{cart.length}</span>
+        🛒
+        <span className="nav-cart-number" data-testid="cart-number">
+          {cart.length}
+        </span>
       </div>
     </nav>
   );
